26e58d3012d2e36bd2fcc2212698fd7e62c4f772
Lucas Souza <lucas.souza59@ufv.br>
2024-06-25 18:20:55 -0300

    projeto base

---
+++ Assets/Scripts/Visual/BetterCardRotation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This script should be attached to the card game object to display card`s rotation correctly.
+/// </summary>
+[ExecuteInEditMode]
+public class BetterCardRotation : MonoBehaviour 
+{
+    // parent game object for all the card face graphics
+    public GameObject cardFront;
+
+    // parent game object for all the card back graphics
+    public GameObject cardBack;
+
+    private Camera main;
+    
+    private void Awake()
+    {
+        main = Camera.main;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var frontFacingCamera = IsCardFrontFacingCamera();
+        
+        cardFront.SetActive(frontFacingCamera);
+        cardBack.SetActive(!frontFacingCamera);
+    }
+
+    private bool IsCardFrontFacingCamera()
+    {
+        return Vector3.Dot(
+            cardFront.transform.forward,
+            main.transform.position - cardFront.transform.position) < 0;
+    }
+}
